91adcacd287e0b4bfe40e08fe1b8bf68ab6b8226
Glevinzon <glevinzon.zeal@gmail.com>
2018-01-13 12:08:44 +0800

    changed ip to local

--- src/routes/Login/components/Login/Login.js
+++ src/routes/Login/components/Login/Login.js
@@ -13,7 +13,7 @@ export default class Login extends Component {
     return (
       <div className='container login-wrapper' style={{ marginTop: '10%' }}>
         <div className='hr-divider m-t m-b'>
-          <h3 className='hr-divider-content hr-divider-heading'><img src='http://192.168.1.195:3000/usep-logo.png' width='70' /></h3>
+          <h3 className='hr-divider-content hr-divider-heading'><img src='http://localhost:3000/usep-logo.png' width='70' /></h3>
         </div>
         <LoginForm {...this.props} />
         <div className='hr-divider'>
