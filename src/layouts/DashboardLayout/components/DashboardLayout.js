efa456f148f5663184abbdba63b156992c0cc05d
Glevinzon D. Dapal <glevinzon.dapal@usep.edu.ph>
2018-01-16 18:12:56 +0800

    fix styles

--- src/layouts/DashboardLayout/components/DashboardLayout.js
+++ src/layouts/DashboardLayout/components/DashboardLayout.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import LoadingBar from 'react-redux-loading-bar'
+import activeComponent from 'react-router-active-component'
+import { Link } from 'react-router'
+
+const NavLink = activeComponent('li')
 
 import 'antd/lib/layout/style/css'
 import 'antd/lib/menu/style/css'
 import 'antd/lib/icon/style/css'
+import 'antd/lib/avatar/style/css'
 import './style.css'
 
-import { Layout, Menu, Icon } from 'antd'
+import { Layout, Menu, Icon, Avatar } from 'antd'
 const { Header, Sider, Content } = Layout
 
 class DashboardLayout extends Component {
@@ -21,6 +26,7 @@ class DashboardLayout extends Component {
   }
 
   render () {
+    let userRole = this.props.user.get('role')
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <LoadingBar style={{position: 'fixed', top: 0, left: 0, backgroundColor: '#800000', zIndex: 9999, height: 5}} />
@@ -28,20 +34,18 @@ class DashboardLayout extends Component {
           trigger={null}
           collapsible
           collapsed={this.state.collapsed}>
-          <div className='logo' />
+          <Link to='/dashboard'><div className='logo' style={{textAlign: 'center', marginBottom: this.state.collapsed ? '1.75em' : '4.5em'}}>
+            <img style={{width: this.state.collapsed ? '100%' : '50%'}} src='http://localhost:3000/usep-logo.png' />
+          </div></Link>
           <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
-            <Menu.Item key='1'>
+            {userRole == 'student' ? <NavLink style={{paddingLeft: '24px'}} className='ant-menu-item' activeClassName='ant-menu-item-active ant-menu-item-selected' key='1' to='/dashboard/me'>
               <Icon type='user' />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key='2'>
-              <Icon type='video-camera' />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key='3'>
-              <Icon type='upload' />
-              <span>nav 3</span>
-            </Menu.Item>
+              <span>Profile</span>
+            </NavLink> : null}
+            {userRole == 'admin' ? <NavLink style={{paddingLeft: '24px'}} className='ant-menu-item' activeClassName='ant-menu-item-active ant-menu-item-selected' key='2' to='/dashboard/accounts'>
+              <Icon type='team' />
+              <span>Accounts</span>
+            </NavLink> : null}
           </Menu>
         </Sider>
         <Layout>
