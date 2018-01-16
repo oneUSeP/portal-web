de8f77e095918e69e5064ab3f6d5c16d3bfe7ba0
Glevinzon <glevinzon.zeal@gmail.com>
2018-01-17 01:18:57 +0800

    changes to profile layout

--- src/routes/Profile/components/Profile/Profile.js
+++ src/routes/Profile/components/Profile/Profile.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
-import ProfileForm from './ProfileForm'
-import upperCase from 'upper-case'
-import Loading from '../../../../components/common/Loading'
+import 'antd/lib/row/style/css'
+import 'antd/lib/col/style/css'
+import 'antd/lib/card/style/css'
+import 'antd/lib/icon/style/css'
+import 'antd/lib/avatar/style/css'
+import 'antd/lib/spin/style/css'
+import './styles.css'
 
+import { Row, Col, Card, Icon, Avatar, Spin } from 'antd'
+const { Meta } = Card
+
+import toUpper from 'upper-case'
 class Profile extends Component {
 
   componentWillMount () {
@@ -16,50 +24,23 @@ class Profile extends Component {
       var image = profile.get('StudentPicture')
     }
     return (
-      <div className='container'>
-        <div className='dashhead'>
-          <div className='dashhead-titles'>
-            <h6 className='dashhead-subtitle'>USEP-KMSD | USEP-Portal</h6>
-            <h3 className='dashhead-title'>Profile</h3>
-          </div>
-        </div>
-
-        <hr className='m-t-0 m-b-sm' />
-
-        <div className='tab-content'>
-          <div className='text-center'><Loading hidden={!fetchingProfile} /></div>
-          {!fetchingProfile ? <div role='tabpanel' className='tab-pane active' id='traffic'>
-            <div className='row m-t-md'>
-              <div className='col-sm-4 m-b-md'>
-                <div className='w-lg m-x-auto'>
-                </div>
-              </div>
-              <div className='col-sm-4 m-b-sm'>
-                <div className='w-lg m-x-auto'>
-                {!fetchingProfile
-                  ? <div style={{position: 'relative', height: '0', paddingBottom: '100%', overflow: 'hidden', borderRadius: '50%'}}><img src={image ? 'data:image/png;base64, ' + image : 'http://localhost:3000/usep-logo.png'} alt='' style={{position: 'absolute', width: '100%', height: '100%', objectFit: 'cover', textAlign: 'left !important'}} /></div>
-                  : null}
-                </div>
-                {!fetchingProfile ? <div className='text-center'>
-                  <strong className='text-muted'>{profile ? profile.get('StudentNo') : 'Empty'}</strong>
-                  <h3>{profile ? upperCase(profile.get('LastName')) + ', ' + profile.get('FirstName') + ' ' + (profile.get('MiddleName') ? profile.get('MiddleName') : '') + (profile.get('ExtName') ? ', ' + profile.get('ExtName') : '') : ''}</h3>
-                </div> : null}
-              </div>
-              <div className='col-sm-4 m-b-md'>
-                <div className='w-lg m-x-auto'>
-                </div>
-              </div>
-            </div>
-          </div> : null}
-        </div>
-
-       {!fetchingProfile ? <div className='hr-divider m-t m-b-lg'>
-          <h3 className='hr-divider-content hr-divider-heading'>Personal Information</h3>
-        </div> : null}
-
-        {!fetchingProfile ? <hr className='m-t-0 m-b-md' /> : null}
-
-      </div>
+      <Row>
+        <Col xs={{ span: 24, offset: 0 }} sm={{ span: 7, offset: 1 }} md={{ span: 7, offset: 1 }} lg={{ span: 6, offset: 0 }} xl={{ span: 4, offset: 0 }}>
+          <Card loading={fetchingProfile}
+            style={{ width: '100%' }}
+            cover={fetchingProfile ? (<div className='example'>
+            <Spin />
+          </div>) : (<img alt='example' src={'data:image/png;base64, ' + profile.get('StudentPicture')} />)}
+            actions={[<Icon type={fetchingProfile ? 'loading' : 'setting'} />, <Icon type={fetchingProfile ? 'loading' : 'edit'} />, <Icon type={fetchingProfile ? 'loading' : 'ellipsis'} />]}>
+            <Meta
+              avatar={<Avatar style={{ backgroundColor: '#f56a00', verticalAlign: 'middle' }} size='large' >{profile.get('FirstName').charAt(0)}</Avatar>}
+              title={toUpper(profile.get('LastName')) + ', ' + profile.get('FirstName') + ' ' + profile.get('MiddleName')}
+              description={profile.get('StudentNo')}
+            />
+          </Card>
+        </Col>
+        <Col xs={{ span: 24, offset: 0 }} sm={{ span: 7, offset: 1 }} md={{ span: 15, offset: 1 }} lg={{ span: 18, offset: 0 }} xl={{ span: 20, offset: 0 }}></Col>
+      </Row>
     )
   }
 }
