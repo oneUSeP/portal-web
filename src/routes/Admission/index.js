88ad6c03ee372afb388d99a6e2cdb94967f3c7e9
Glevinzon D. Dapal <glevinzon.dapal@usep.edu.ph>
2018-01-23 11:17:36 +0800

    removed admissions

--- src/routes/Admission/index.js
+++
@@ -1,7 +0,0 @@
-import AdmissionContainer from './containers/AdmissionContainer'
-import { UserIsAuthenticated, UserIsAdmin } from 'utils/authWrappers'
-
-export default (store) => ({
-  path: 'admissions',
-  component: UserIsAuthenticated(UserIsAdmin(AdmissionContainer))
-})
